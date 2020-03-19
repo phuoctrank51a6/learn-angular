27cbfac6c5176a8f975cec6d6f3857d8059c4aa7
phuoctrank51a6 <phuoctrank51a6@gmail.com>
2020-03-19 14:12:38 +0700

    2 files modified

--- src/app/MockProduct.ts
+++ src/app/MockProduct.ts
@@ -4,7 +4,7 @@ import { Product } from "./Product";
     {
       id: 1,
       name: "item 1",
-      price: 25.25,
+      price: 25.1111,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -13,7 +13,7 @@ import { Product } from "./Product";
     {
       id: 2,
       name: "item 2",
-      price: 25.522,
+      price: 25.222,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -22,7 +22,7 @@ import { Product } from "./Product";
     {
       id: 3,
       name: "item 3",
-      price: 25.88,
+      price: 25.3333,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -31,7 +31,7 @@ import { Product } from "./Product";
     {
       id: 4,
       name: "item 4",
-      price: 25.25,
+      price: 25.44,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -40,7 +40,7 @@ import { Product } from "./Product";
     {
       id: 5,
       name: "item 5",
-      price: 25.522,
+      price: 25.55,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -49,7 +49,7 @@ import { Product } from "./Product";
     {
       id: 6,
       name: "item 6",
-      price: 25.88,
+      price: 25.666,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -58,7 +58,7 @@ import { Product } from "./Product";
     {
       id: 7,
       name: "item 7",
-      price: 25.25,
+      price: 25.77,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -67,7 +67,7 @@ import { Product } from "./Product";
     {
       id: 8,
       name: "item 8",
-      price: 25.522,
+      price: 25.88,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -76,7 +76,7 @@ import { Product } from "./Product";
     {
       id: 9,
       name: "item 9",
-      price: 25.88,
+      price: 25.99,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
@@ -85,7 +85,7 @@ import { Product } from "./Product";
     {
       id: 10,
       name: "item 10",
-      price: 25.88,
+      price: 25.10,
       desc: "Nội dung mô tả sản phẩm",
       img:
         "https://dummyimage.com/300x300/343a40/fff",
