7ad357be484eb80671d183dd4a5b26346fa3dff6
phuoctrank51a6 <phuoctrank51a6@gmail.com>
2020-03-20 16:40:45 +0700

    5 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
 
 
 const routes: Routes = [
+  { path: '**', component: ListProductComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: ListProductComponent},
+  { path: 'product-manager', component: ProductManagerComponent},
   { path: 'add-product' ,component: ProductAddComponent}
 ];
 
