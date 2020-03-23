8f62e9f935b3f75da5aa6c55e65255b15d7cf36c
phuoctrank51a6 <phuoctrank51a6@gmail.com>
2020-03-23 17:57:36 +0700

    1 file modified, 6 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ServiceComponent } from './service/service.component';
+// import { AboutComponent } from './about/about.component';
+// import { ServiceComponent } from './service/service.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
@@ -12,8 +12,8 @@ import { ProductManagerComponent } from './product-manager/product-manager.compo
 const routes: Routes = [
   // { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: ListProductComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'service' ,component: ServiceComponent},
+  // { path: 'about', component: AboutComponent},
+  // { path: 'service' ,component: ServiceComponent},
   { path: 'product-manager', component: ProductManagerComponent},
   { path: 'add-product' ,component: ProductAddComponent},
   { path: 'edit-product/:id' ,component: EditProductComponent},
