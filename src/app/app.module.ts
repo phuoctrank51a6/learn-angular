580c536dd4df98a544fef19b74c96b39e09d9d17
phuoctrank51a6 <phuoctrank51a6@gmail.com>
2020-03-23 17:53:54 +0700

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,11 @@ import { ProductManagerComponent } from './product-manager/product-manager.compo
 import { ProductService } from './product.service';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
-import { AboutComponent } from './about/about.component';
-import { ServiceComponent } from './service/service.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, SlideshowComponent, CategoryComponent, ListProductComponent, ItemProductComponent, ProductManagerComponent, ProductAddComponent, EditProductComponent, AboutComponent, ServiceComponent, DetailProductComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideshowComponent, CategoryComponent, ListProductComponent, ItemProductComponent, ProductManagerComponent, ProductAddComponent, EditProductComponent, DetailProductComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
